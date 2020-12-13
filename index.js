2ba09e6293752d59ee9f5874c91645a1952b25ac
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2020-12-13 16:52:54 +0100

    remove: limited access by ip

--- index.js
+++ index.js
@@ -23,22 +23,6 @@ mongoose.connect(
 //Middlewares 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
-app.use('/', function(req, res, next) {
-  // do your filtering here, call a `res` method if you want to stop progress or call `next` to proceed
-  var ip = req.ip || 
-           req.headers['x-forwarded-for'] || 
-           req.connection.remoteAddress || 
-           req.socket.remoteAddress ||
-           req.connection.socket.remoteAddress;
-
-   // Your allowed ip. You can also check against an array
-   console.log(ip)
-   if (ip == process.env.IP_ADDRESS) {
-     next();
-   } else {
-      res.send("No access to the application")
-   }
-})
 
 //Routes Middleware
 // app.use("/api/user", authRoutes);
